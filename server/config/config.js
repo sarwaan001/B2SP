0c92cd6724b7d6caf7386273ac11c5f71254c523
bchevali <b.chevalier@ufl.edu>
2019-03-22 11:51:46 -0400

    Initial API testing with twit node package

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,13 @@
 //'config.js' is typically ignored by git to protect sensitive information, such as your database's username and password
 
 module.exports = {
-  db: {
+  /*db: {
     uri: '', //place the URI of your mongo database here.
   }, 
-  port: 8080
+  port: 8080*/
+
+  consumer_key: 'kd2Srldv4Z6j5HXdJlHHjeDZu',
+  consumer_secret:  'z4NeNEf2uRZBXHvVdlbJWVVelDN4Gz83hyrbBet2w1tC00svX1',
+  access_token: '1101176210640515072-bkJ29E0W6OHyo938KqSo4sk5sdDq6k',
+  access_token_secret:  'JEMOlcPV2evpMgewUquVGW6s0tk3siqAZjnsqkaXvolSF'
 };
