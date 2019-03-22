51dbec6ff91afc426cfd6f4d542cb999f4dba14b
bchevali <b.chevalier@ufl.edu>
2019-03-22 12:20:45 -0400

    Fixed issue with config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,10 @@
 //'config.js' is typically ignored by git to protect sensitive information, such as your database's username and password
 
 module.exports = {
-  /*db: {
+  db: {
     uri: '', //place the URI of your mongo database here.
   }, 
-  port: 8080*/
+  port: 8080,
 
   consumer_key: 'kd2Srldv4Z6j5HXdJlHHjeDZu',
   consumer_secret:  'z4NeNEf2uRZBXHvVdlbJWVVelDN4Gz83hyrbBet2w1tC00svX1',
